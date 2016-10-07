1fce60bd82ab845c55aa2ebd7fc18dd4e8946667
antonnn1 <antonnn@vgy.se>
2016-10-07 14:46:28 +0200

    ovn2

--- StringFormatering/Program.cs
+++ StringFormatering/Program.cs
@@ -10,10 +10,17 @@ namespace StringFormatering
     {
         static void Main(string[] args)
         {
-
+            /*
             //Övn 1
             DateTime dt = new DateTime(2016, 10, 7);
             Console.WriteLine(dt.ToString("d"));
+            */
+
+            //Övn 2
+            double area = Math.PI * 4 * 4;
+            Console.WriteLine(area.ToString("F4"));
+
+
         }
     }
 }
