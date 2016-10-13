851c288ecb8c86d5c335ad6021c6b169a4158892
antonnn1 <antonnn@vgy.se>
2016-10-13 19:10:21 +0200

    ovn6

--- StringFormatering/Program.cs
+++ StringFormatering/Program.cs
@@ -34,11 +34,24 @@ namespace StringFormatering
             Console.WriteLine("Det blir {0} kr och {1:00} öre", value, 00);
             */
 
+            /*
             //Övn 5
             Console.WriteLine("Mata in ditt person nummer: ");
             string pn = Console.ReadLine();
             decimal pndelat = decimal.Parse(pn);
             Console.WriteLine("{0:#### ## ##-####}", pndelat);
+            */
+
+            //Övn 6
+            const string myForm = "{0,-10} | {1,-10} | {2,-10} | {3,-10} | {4,-10}";
+            Console.WriteLine("----------------------------Veckoschema------------------------");
+            Console.WriteLine(String.Format(myForm, "Måndag", "Tisdag", "Onsdag", "Torsdag", "Fredag"));
+            Console.WriteLine("---------------------------------------------------------------");
+
+            Console.WriteLine(String.Format(myForm, "Fysik", "Inbokat", "Matte", "Franska", "Matte"));
+            Console.WriteLine(String.Format(myForm, "Matte", "Inbokat", "Teknik", "Matte", "Webb"));
+            Console.WriteLine(String.Format(myForm, "Franska", "Inbokat", "Idrott", "Engelska", "Prog"));
+            Console.WriteLine(String.Format(myForm, "", "", "Svenska", "Fysik", "Engelska"));
         }
     }
 }
